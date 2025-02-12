b6f9018189a9b603d8167e413f4acbf3e26db16a
Henrique MS <henrique-ms@outlook.com>
2025-02-12 19:08:09 +0000

    update quickfix list keymaps

--- lua/riquems/remap.lua
+++ lua/riquems/remap.lua
@@ -4,5 +4,11 @@ vim.keymap.set({ "i", "n", "v" }, "<C-s>", function()
     vim.cmd("w")
 end)
 
+-- doesn't work because Shift + Tab conflicts with CTRL + I in terminals
 -- vim.keymap.set({ "i", "n", "v" }, "<Tab>", "<cmd>cnext<CR>")
-vim.keymap.set({ "i", "n", "v" }, "<S-Tab>", "<cmd>cprev<CR>")
+-- vim.keymap.set({ "i", "n", "v" }, "<S-Tab>", "<cmd>cprev<CR>")
+
+vim.keymap.set({ "n" }, "]q", "<cmd>cnext<CR>")
+vim.keymap.set({ "n" }, "[q", "<cmd>cprev<CR>")
+vim.keymap.set({ "n" }, "qo", "<cmd>copen<CR>")
+vim.keymap.set({ "n" }, "qc", "<cmd>cclose<CR>")
