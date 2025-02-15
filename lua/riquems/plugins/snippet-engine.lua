b56fcd2b1fa916d9a2b2e574852b030c34018627
Henrique Souza <henrique-ms@outlook.com>
2025-02-15 20:04:32 -0300

    remove LuaSnip keybindings
    the tab one would insert a raw tab character even if expandtab option is on, which is annoying

--- lua/riquems/plugins/snippet-engine.lua
+++ lua/riquems/plugins/snippet-engine.lua
@@ -16,9 +16,5 @@ return {
             -- Uncomment to enable visual snippets triggered using <c-x>
             -- store_selection_keys = '<c-x>',
         })
-        -- LuaSnip key bindings
-        vim.keymap.set({"i", "s"}, "<Tab>", function() if ls.expand_or_jumpable() then ls.expand_or_jump() else vim.api.nvim_input('<C-V><Tab>') end end, {silent = true})
-        vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true})
-        vim.keymap.set({"i", "s"}, "<C-E>", function() if ls.choice_active() then ls.change_choice(1) end end, {silent = true})
     end
 }
