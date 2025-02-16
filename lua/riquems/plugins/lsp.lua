3283c9f5435de995be4a92c5c2561c970ac0f5d8
Henrique Souza <henrique-ms@outlook.com>
2025-02-15 23:51:58 -0300

    remove cmp unused mappings, add enter as confirm

--- lua/riquems/plugins/lsp.lua
+++ lua/riquems/plugins/lsp.lua
@@ -78,10 +78,8 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
             }),
             sources = {
                 { name = 'nvim_lsp' },
