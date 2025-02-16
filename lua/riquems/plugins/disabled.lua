f3831885e0527cc60ced872ccd3205db57bb57dc
Henrique Souza <henrique-ms@outlook.com>
2025-02-15 23:08:56 -0300

    delete disabled plugins file

--- lua/riquems/plugins/disabled.lua
+++
@@ -1,8 +0,0 @@
-return {
---    { "Luxed/ayu-vim", enabled = false },
---    { "mireq/luasnip-snippets", enabled = false },
---    { "L3MON4D3/LuaSnip", enabled = false },
---    { "neovim/nvim-lspconfig", enabled = false },
---    { "Luxed/ayu-vim", enabled = false },
---    { "Luxed/ayu-vim", enabled = false },
-}
