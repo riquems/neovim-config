ffdb99442179632796c3972ed85a99851f44370b
Henrique Souza <henrique-ms@outlook.com>
2025-04-19 23:41:31 -0300

    add lsp signature help

--- lua/riquems/plugins/cmp.lua
+++ lua/riquems/plugins/cmp.lua
@@ -3,6 +3,7 @@ return {
    { "hrsh7th/cmp-buffer" },
    { "hrsh7th/cmp-path" },
    { "hrsh7th/cmp-cmdline" },
+   { "hrsh7th/cmp-nvim-lsp-signature-help" },
    { "saadparwaiz1/cmp_luasnip" },
    {
       "hrsh7th/nvim-cmp",
@@ -21,6 +22,7 @@ return {
             }),
             sources = {
                { name = 'nvim_lsp' },
+               { name = 'nvim_lsp_signature_help' },
                { name = 'buffer' },
                { name = 'lazydev' },
                { name = 'luasnip' },
