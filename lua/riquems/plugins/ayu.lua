ae142e45d9fd1404ab08a6612eb33c4c901badcc
Henrique Souza <henrique-ms@outlook.com>
2025-09-16 22:22:03 -0300

    add custom diff highlights

--- lua/riquems/plugins/ayu.lua
+++ lua/riquems/plugins/ayu.lua
@@ -3,5 +3,11 @@ return {
    config = function()
       vim.opt.termguicolors = true
       vim.cmd.colorscheme("ayu")
+
+      vim.api.nvim_set_hl(0, 'DiffNormal', { fg = '#888888', bg = '#0f1419' })
+      vim.api.nvim_set_hl(0, 'DiffChange', { fg = '#888888' })
+      vim.api.nvim_set_hl(0, 'DiffText', { fg = '#FF9900', bg = '#222200' })
+      vim.api.nvim_set_hl(0, 'DiffAdd', { fg = '#00EE00', bg = '#002200' })
+      vim.api.nvim_set_hl(0, 'DiffDelete', { fg = '#FF5555' })
    end
 }
